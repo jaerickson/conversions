2b788df8329905b39f374f1a762586b70f88c8f0
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-10-10 09:18:58 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/response")
+def render_response():
+    number = request.args['number']
+    #The request object stores information about the request sent to the server. 
+    #args is a MultiDict (like a dictionary bu tcan have multiple values for the same key)
+    #The information in args is visible in the url for the page being requested (ex. .../response?color=blue
+    if color == 'purple' :
+        reply = "That's my favorite color,too!"
+    else:
+        reply = "My favorite color is purple."
+    return render_template('response.html', response = reply)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
