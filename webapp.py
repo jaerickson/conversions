78e665733a124557f026e4f8a9d9e07c49cd7330
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-10-12 09:02:41 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,9 +1,7 @@
 from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) 
-@app.route("/")
-def render_main():
-    return render_template('home.html')
+
 
 @app.route("/")
 def render_main():
