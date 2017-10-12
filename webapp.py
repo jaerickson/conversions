557e7bdf0f1e02d583d9610338773f3444fd338c
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-10-12 08:50:44 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('dollartoeuro.html')
 
 @app.route("/response")
 def render_response():
@@ -12,10 +12,7 @@ def render_response():
     #The request object stores information about the request sent to the server. 
     #args is a MultiDict (like a dictionary bu tcan have multiple values for the same key)
     #The information in args is visible in the url for the page being requested (ex. .../response?color=blue
-    if color == 'purple' :
-        reply = "That's my favorite color,too!"
-    else:
-        reply = "My favorite color is purple."
+    reply = float(request.args['number']*.85)   
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
