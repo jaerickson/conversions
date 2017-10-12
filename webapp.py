9b4c1427921a6edfb829f8c4e4ee83dcf4566301
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-10-12 09:00:32 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) 
+@app.route("/")
+def render_main():
+    return render_template('home.html')
 
 @app.route("/")
 def render_main():
@@ -10,6 +13,32 @@ def render_main():
 def render_response():
     euro = float(request.args['euro'])
     reply = euro*.85   
+    return render_template('response.html', response = reply)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
+    
+@app.route("/")
+def render_main():
+    return render_template('dollartosol.html')
+
+@app.route("/response")
+def render_response():
+    sol = float(request.args['sol'])
+    reply = sol*3.26
+    return render_template('response.html', response = reply)
+    
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
+    
+@app.route("/")
+def render_main():
+    return render_template('dollartofranc.html')
+
+@app.route("/response")
+def render_response():
+    franc = float(request.args['franc'])
+    reply = franc*.97
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
